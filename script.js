bf017e3506fe50a6f3d8fd14215ee147e472a8ac
Chi Anh Bui <chrisbui.dev188@gmail.com>
2024-04-26 22:40:21 -0400

    Organize code

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const detailsElements = document.querySelectorAll('details');
+document.addEventListener('DOMContentLoaded', () => {
+  const detailsElements = document.querySelectorAll('details');
 
-Array.from(detailsElements).forEach((details) => {
-  details.addEventListener('toggle', () => {
-    const plusIcon = details.querySelector('.plus-icon');
-    const minusIcon = details.querySelector('.minus-icon');
+  Array.from(detailsElements).forEach((details) => {
+    details.addEventListener('toggle', () => {
+      const plusIcon = details.querySelector('.plus-icon');
+      const minusIcon = details.querySelector('.minus-icon');
 
-    if (details.open) {
-      plusIcon.className = 'plus-icon hide';
-      minusIcon.className = 'minus-icon';
-    } else {
-      plusIcon.className = 'plus-icon';
-      minusIcon.className = 'minus-icon hide';
-    }
+      if (details.open) {
+        plusIcon.className = 'plus-icon hide';
+        minusIcon.className = 'minus-icon';
+      } else {
+        plusIcon.className = 'plus-icon';
+        minusIcon.className = 'minus-icon hide';
+      }
+    });
   });
 });
